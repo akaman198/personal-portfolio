95bea8675b7d2876157e9e5d5a63855e0aa9aa58
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-05-29 11:45:27 +0200

    add other projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,6 +12,7 @@ import {
   DiMongodb,
   DiBootstrap,
   DiGitBranch,
+  DiGithubBadge,
 } from "react-icons/di";
 
 const About = () => {
@@ -38,6 +39,80 @@ const About = () => {
             alt="project1"
             className="rounded-lg md:max-w-[500px]"
           />
+        </div>
+      </div>
+
+      <div className="p-6">
+        <h2 className="text-gray-200 text-3xl font-bold mb-4">Skills</h2>
+        <p className="text-gray-300 mb-4">
+          As a passionate web front-end developer with over 2 years of
+          experience, I have a proven track record of creating visually stunning
+          and responsive websites.
+        </p>
+        <div className="md:flex flex-wrap text-4xl justify-center hidden">
+          <DiHtml5 className="text-orange-600" />
+          <DiCss3 className="text-blue-600" />
+          <DiSass className="text-pink-600" />
+          <DiBootstrap className="text-purple-600" />
+          <DiJavascript1 className="text-yellow-500" />
+          <DiReact className="text-blue-500" />
+          <DiNodejsSmall className="text-green-500" />
+        </div>
+      </div>
+
+      <div className="p-6">
+        <h2 className="text-gray-200 text-3xl font-bold mb-4">
+          Front End Experience
+        </h2>
+        <p className="text-gray-300 mb-4">
+          Experienced in creating modern, responsive web applications using the
+          latest frontend technologies.
+        </p>
+        <div className="md:flex flex-wrap text-4xl justify-center hidden">
+          <DiHtml5 className="text-orange-600" />
+          <DiCss3 className="text-blue-600" />
+          <DiSass className="text-pink-600" />
+          <DiBootstrap className="text-purple-600" />
+          <DiJavascript1 className="text-yellow-500" />
+          <DiReact className="text-blue-500" />
+        </div>
+      </div>
+
+      <div className="relative group">
+        <div className="w-full h-full absolute -inset-1 bg-gradient-to-r from-teal-100 to-teal-900 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-300"></div>
+        <div className="relative w-full p-4 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg">
+          <img
+            src={project2}
+            alt="project2"
+            className="rounded-lg md:max-w-[500px]"
+          />
+        </div>
+      </div>
+
+      <div className="relative group">
+        <div className="w-full h-full absolute -inset-1 bg-gradient-to-r from-teal-100 to-teal-900 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-300"></div>
+        <div className="relative w-full p-4 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg">
+          <img
+            src={project3}
+            alt="project3"
+            className="rounded-lg md:max-w-[500px]"
+          />
+        </div>
+      </div>
+
+      <div className="p-6">
+        <h2 className="text-gray-200 text-3xl font-bold mb-4">
+          Full Stack Experience
+        </h2>
+        <p className="text-gray-300 mb-4">
+          Skilled in developing end-to-end solutions using fullstack
+          technologies.
+        </p>
+        <div className="md:flex flex-wrap text-4xl justify-center hidden">
+          <DiNodejsSmall className="text-green-500" />
+          <DiMongodb className="text-green-500" />
+          <DiReact className="text-blue-500" />
+          <DiGithubBadge className="text-gray-500" />
         </div>
       </div>
     </div>
