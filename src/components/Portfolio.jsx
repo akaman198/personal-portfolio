519c183d46cbd7015454bb3573b62c999186fe08
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-05-29 12:38:03 +0200

    make projects dynamic

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -20,7 +20,7 @@ const projects = [
   {
     img: project2,
     title: "project #2",
-    description: "UI for front end development",
+    description: "A fullstack application built with Node.js and MongoDB.",
     links: {
       site: "#",
       github: "#",
@@ -29,7 +29,7 @@ const projects = [
   {
     img: project3,
     title: "project #3",
-    description: "UI for front end development",
+    description: "A responsive website built with morden css",
     links: {
       site: "#",
       github: "#",
@@ -38,7 +38,7 @@ const projects = [
   {
     img: project4,
     title: "project #4",
-    description: "UI for front end development",
+    description: "An e-commerce platform with various featues ",
     links: {
       site: "#",
       github: "#",
@@ -47,7 +47,7 @@ const projects = [
   {
     img: project5,
     title: "project #5",
-    description: "UI for front end development",
+    description: "A mobile friendly application using react native",
     links: {
       site: "#",
       github: "#",
@@ -56,7 +56,7 @@ const projects = [
   {
     img: project6,
     title: "project #6",
-    description: "UI for front end development",
+    description: "A data visualization project using D3.js and other libraries",
     links: {
       site: "#",
       github: "#",
@@ -65,7 +65,57 @@ const projects = [
 ];
 
 const Portfolio = () => {
-  return <div></div>;
+  const [currentProject, setCurrentProject] = useState(0);
+
+  return (
+    <div
+      className="relative max-w-[800px] mx-auto p-6 md:my-20 flex flex-col md:flex-row"
+      id="portfolio"
+    >
+      <div className="glass p-6 w-full border-2 max-w-[600px]">
+        <div className="w-full h-80">
+          <img
+            src={projects[currentProject].img}
+            alt={projects[currentProject].title}
+            className="w-full h-full object-cover rounded-lg mb-4"
+          />
+        </div>
+
+        <p className="text-gray-200 my-4">
+          {projects[currentProject].description}
+        </p>
+
+        <div className="flex space-x-4">
+          <a
+            href={projects[currentProject].links.site}
+            className="px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300"
+          >
+            View Site
+          </a>
+          <a
+            href={projects[currentProject].links.github}
+            className="px-4 py-2 bg-gray-800 text-gray-200 text2xl rounded-lg hover:bg-gray-600 transition duration-300"
+          >
+            <AiFillGithub />
+          </a>
+        </div>
+      </div>
+
+      <ul className="ml-6 flex flex-row md:flex-col gap-6 flex-wrap justify-center mt-4 md:gap-1">
+        {projects.map((project, index) => (
+          <li
+            key={index}
+            onClick={() => setCurrentProject(index)}
+            className={`cursor-pointer text-gray-300 bg-slate-700 rounded-lg p-2 max-w-[100px] hover:bg-slate-800 transition duration-300 ${
+              currentProject === index ? "bg-slate-900" : ""
+            }`}
+          >
+            {project.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Portfolio;
