af4ff72947fb58bce4ee45894ac2dc2f9d96fbf8
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-05-29 11:11:37 +0200

    add react icons on about sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import project1 from "../assets/project1.png";
+import project2 from "../assets/project2.png";
+import project3 from "../assets/project7.png";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejsSmall,
+  DiHtml5,
+  DiCss3,
+  DiSass,
+  DiMongodb,
+  DiBootstrap,
+  DiGitBranch,
+} from "react-icons/di";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div
+      className="max-w-[1300px] mx-auto p-6 grid md:grid-cols-2 gap-8 place-items-center"
+      id="about"
+    >
+      <div className="bg-white bg-opacity-10 backdrop-blur-lg rounded-lg p-6 flex flex-wrap gap-4 text-4xl justify-center">
+        <DiHtml5 className="text-orange-600" />
+        <DiCss3 className="text-blue-600" />
+        <DiSass className="text-pink-600" />
+        <DiBootstrap className="text-purple-600" />
+        <DiJavascript1 className="text-yellow-500" />
+        <DiReact className="text-blue-500" />
+        <DiNodejsSmall className="text-green-500" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
