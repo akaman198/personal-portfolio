88ae1535ceafb932324b67fc9b73b51f1bfc5db1
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-05-29 11:22:45 +0200

    add project 1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
       className="max-w-[1300px] mx-auto p-6 grid md:grid-cols-2 gap-8 place-items-center"
       id="about"
     >
-      <div className="bg-white bg-opacity-10 backdrop-blur-lg rounded-lg p-6 flex flex-wrap gap-4 text-4xl justify-center">
+      <div className="md:hidden bg-white bg-opacity-10 backdrop-blur-lg rounded-lg p-6 flex flex-wrap gap-4 text-4xl justify-center">
         <DiHtml5 className="text-orange-600" />
         <DiCss3 className="text-blue-600" />
         <DiSass className="text-pink-600" />
@@ -28,6 +28,17 @@ const About = () => {
         <DiJavascript1 className="text-yellow-500" />
         <DiReact className="text-blue-500" />
         <DiNodejsSmall className="text-green-500" />
+      </div>
+
+      <div className="relative group">
+        <div className="w-full h-full absolute -inset-1 bg-gradient-to-r from-teal-100 to-teal-900 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-300"></div>
+        <div className="relative w-full p-4 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg">
+          <img
+            src={project1}
+            alt="project1"
+            className="rounded-lg md:max-w-[500px]"
+          />
+        </div>
       </div>
     </div>
   );
