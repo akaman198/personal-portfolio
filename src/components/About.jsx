4b02d54c6136a3dc1a0a1662d3852d7ee6866f67
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-05-31 13:10:21 +0200

    remove full stack experience from about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -87,33 +87,6 @@ const About = () => {
           />
         </div>
       </div>
-
-      <div className="relative group">
-        <div className="w-full h-full absolute -inset-1 bg-gradient-to-r from-teal-100 to-teal-900 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-300"></div>
-        <div className="relative w-full p-4 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg">
-          <img
-            src={project3}
-            alt="project3"
-            className="rounded-lg md:max-w-[500px]"
-          />
-        </div>
-      </div>
-
-      <div className="p-6">
-        <h2 className="text-gray-200 text-3xl font-bold mb-4">
-          Full Stack Experience
-        </h2>
-        <p className="text-gray-300 mb-4">
-          Skilled in developing end-to-end solutions using fullstack
-          technologies.
-        </p>
-        <div className="md:flex flex-wrap text-4xl justify-center hidden">
-          <DiNodejsSmall className="text-green-500" />
-          <DiMongodb className="text-green-500" />
-          <DiReact className="text-blue-500" />
-          <DiGithubBadge className="text-gray-500" />
-        </div>
-      </div>
     </div>
   );
 };
